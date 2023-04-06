3aa49ff0cffddb5a4bf56d4ec1d5791640eed392
Dharamendra singh <45919483+dhrm9@users.noreply.github.com>
2023-04-06 08:53:56 +0530

    Added solution - LeetHub

--- Reverse words in a given string - GFG/reverse-words-in-a-given-string.cpp
+++ Reverse words in a given string - GFG/reverse-words-in-a-given-string.cpp
@@ -15,17 +15,19 @@ class Solution
       string ans="";
       while(i<n){
           while(i<n && s[i]=='.') i++;
-          if(i>=n) break;
+          if(i>=n)
+          break;
           int j=i+1;
           while(j<n && s[j]!='.') j++;
           string sub=s.substr(i,j-i);
           if(ans.length()==0) ans=sub;
           else
-          ans=sub+'.'+ans;
-          i=j+1;
+              ans=sub+"."+ans;
+              i=j+1;
+          
       }
       return ans;
-        
+      
     } 
 };
 
